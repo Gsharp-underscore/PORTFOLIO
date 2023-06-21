41e11b7a4ba5283fa37476e4ee1b6c9fd58da2bc
Pavithran2 <pavithrank02@gmail.com>
2023-06-21 18:38:55 +0530

    overall UI completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SharedLayout from './components/SharedLayout';
 import Home from './components/Home'
 import About from './components/About'
-// import Projects from './components/Projects'
+import Projects from './components/CurrenProjects'
 import Contact from './components/Contact';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -15,7 +15,7 @@ function App() {
         <Route path='/' element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About/>} />
-          {/* <Route path='projects' element={<Projects />} /> */}
+          <Route path='projects' element={<Projects />} />
           <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
