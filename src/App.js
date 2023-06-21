a18b8446516e74a284683b711926e94a3c524516
Pavithran2 <pavithrank02@gmail.com>
2023-06-21 11:18:09 +0530

    animation and navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import SharedLayout from './components/SharedLayout';
+// import SharedLayout from './components/SharedLayout';
 import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact';
+// import About from './components/About'
+// import Projects from './components/Projects'
+// import Contact from './components/Contact';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -12,12 +12,12 @@ function App() {
      <>
      <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SharedLayout />}>
+        {/* <Route path='/' element={<SharedLayout />}> */}
           <Route index element={<Home />} />
-          <Route path='about' element={<About/>} />
-          <Route path='projects' element={<Projects />} />
-          <Route path='contact' element={<Contact />} />
-        </Route>
+          {/* <Route path='about' element={<About/>} /> */}
+          {/* <Route path='projects' element={<Projects />} />
+          <Route path='contact' element={<Contact />} /> */}
+        {/* </Route> */}
       </Routes>
      </BrowserRouter>
      </>
