146d351e21fdb655d4ecde7982e8b1f3c493b4e5
Pavithran2 <pavithrank02@gmail.com>
2023-06-23 10:59:19 +0530

    routes created

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,29 @@
+import SharedLayout from './components/SharedLayout';
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/CurrenProjects'
+import Contact from './components/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+ 
+   return(
+     <>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About/>} />
+          <Route path='projects' element={<Projects />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+      </Routes>
+     </BrowserRouter>
+     </>
+  )
+ 
+   
 }
 
 export default App;
